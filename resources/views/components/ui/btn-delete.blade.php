7d8e3b71222764719b287e1e03cb26daa5339bcf
Akmal <akmalthemalong@gmail.com>
2026-06-15 07:00:28 +0800

    style: icon dan label menjadi 1 baris

--- resources/views/components/ui/btn-delete.blade.php
+++ resources/views/components/ui/btn-delete.blade.php
@@ -6,10 +6,11 @@
 ])
 
 @php
+    $baseStyle = 'display: inline-flex; align-items: center; justify-content: center; white-space: nowrap; ';
     $sizeStyles = [
-        'sm' => 'font-size: 0.8rem;' . ($iconOnly ? ' width: 32px; height: 32px; padding: 0; display: inline-flex; align-items: center; justify-content: center;' : ''),
-        'md' => 'font-size: 0.9rem;' . ($iconOnly ? ' width: 38px; height: 38px; padding: 0; display: inline-flex; align-items: center; justify-content: center;' : ''),
-        'lg' => 'font-size: 1rem;' . ($iconOnly ? ' width: 44px; height: 44px; padding: 0; display: inline-flex; align-items: center; justify-content: center;' : ''),
+        'sm' => $baseStyle . 'font-size: 0.8rem;' . ($iconOnly ? ' width: 32px; height: 32px; padding: 0;' : ''),
+        'md' => $baseStyle . 'font-size: 0.9rem;' . ($iconOnly ? ' width: 38px; height: 38px; padding: 0;' : ''),
+        'lg' => $baseStyle . 'font-size: 1rem;' . ($iconOnly ? ' width: 44px; height: 44px; padding: 0;' : ''),
     ];
 
     $btnSize = $sizeStyles[$size] ?? $sizeStyles['sm'];
